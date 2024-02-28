c5cb28b65e003d29116c33be578053eec42d7127
Alfredo Lillo <alphonselillo@gmail.com>
2024-02-28 18:17:32 -0300

    Primeros pasos Fundamentos

---
+++ app.js
@@ -0,0 +1,11 @@
+let a = 25,
+  b = 10,
+  c = 65,
+  d = 78;
+
+// console.log({ a });
+// console.warn({ b });
+// console.error({ c });
+// console.log("%c Agregando estilos al LOG", "color: red; font-weight: bold"); // Agregamos estilos CSS a los logs
+
+// console.table({ a, b, c, d }); // Nos crea una tabla en la consola que nos muestra las variables y sus valores

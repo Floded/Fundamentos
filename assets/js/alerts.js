3de230090125d8e429eefc5c10df65f659d88695
Alfredo Lillo <alphonselillo@gmail.com>
2024-02-28 19:34:04 -0300

    agregando cambios

---
+++ assets/js/alerts.js
@@ -0,0 +1,7 @@
+// alert("Hola mundo");
+
+// let siONo = prompt("¿Comiste hoy?", "Default");
+
+// console.log(siONo);
+
+// const selection = confirm("¿Estas seguro de avanzar?"); // retorna un booleano
--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,20 @@
-let a = 25,
-  b = 10,
+// alert("alerta emergente");
+
+let a = "Hola ",
+  b = "gente",
   c = 65,
-  d = 78;
+  d = 78,
+  x = c + d;
+
+const saludo = a + b;
 
 // console.log({ a });
 // console.warn({ b });
 // console.error({ c });
 // console.log("%c Agregando estilos al LOG", "color: red; font-weight: bold"); // Agregamos estilos CSS a los logs
 
+c = "Hola remix";
+
 // console.table({ a, b, c, d }); // Nos crea una tabla en la consola que nos muestra las variables y sus valores
+
+var miNombre = "Luis";

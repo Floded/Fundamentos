34b465996eeb5e1480d15e3e70f30598856ad128
Alfredo Lillo <alphonselillo@gmail.com>
2024-02-28 19:56:35 -0300

    tipos de datos

---
+++ assets/js/primitivos.js
@@ -0,0 +1,30 @@
+let nombre = "Kakashi";
+
+console.log(typeof nombre);
+
+let esAnime = true;
+
+console.log(typeof esAnime);
+
+let edad = 36;
+
+console.log(typeof edad);
+
+let pi = 3.1416;
+
+console.log(typeof pi);
+
+let camelCase;
+
+console.log(typeof camelCase); // undefine
+
+let whatNull = null;
+
+console.log(typeof whatNull); // Object
+
+let symbol1 = Symbol("a");
+let symbol2 = Symbol("a");
+
+console.log(typeof symbol1);
+
+console.log(symbol1 === symbol2);
